6290a42db9fdc60ee09971c0a77d15b5756d8fbd
dewo952 <kabirkhatri1998@gmail.com>
2023-06-08 15:24:47 +0530

    updates

--- src/pages/Industry/TravelTransportationAndHospitality.jsx
+++ src/pages/Industry/TravelTransportationAndHospitality.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import TravelHeader from "../../components/IndustryPage/TravelTransportationAndHospitality/TravelHeader";
 import TravelMainSection from "../../components/IndustryPage/TravelTransportationAndHospitality/TravelMainSection";
 import TravelCoreSolution from "../../components/IndustryPage/TravelTransportationAndHospitality/TravelCoreSolution";
-import TravelSuccesStory from "../../components/IndustryPage/TravelTransportationAndHospitality/TravelSuccesStory";
-import BankingSuccesStory from "../../components/IndustryPage/BankingInsuranceAndFinancialServices/BankingSuccesStory";
 import SuccessStorySection from "../../components/ServicePage/SuccessStorySection";
 
 const TravelTransportationAndHospitality = () => {
